28428539ced47f88e7a0a49cac4b179c6d801a2c
aadltya <adityaedu22@gmail.com>
2024-04-09 19:00:16 +0530

    added

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ export interface Env {
 	//
 	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
 	// MY_QUEUE: Queue;
+	//all set
 }
 
 export default {
